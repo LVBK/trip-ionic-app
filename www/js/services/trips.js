841814eee93ffe73d3b3471e3fd34058873e5f85
LVBK <khanhlvb@ows.vn>
2016-05-04 00:03:48 +0700

    trip update, delete service

--- www/js/services/trips.js
+++ www/js/services/trips.js
@@ -90,9 +90,48 @@ angular.module('app.services.trips', [])
     createATrip = function (param, callback) {
       Meteor.call('createATrip', param, callback);
     };
+    updateTrip = function (tripId, modifier, callback) {
+      Meteor.call('updateTrip', tripId, modifier, callback);
+    };
+    deleteTrip = function (tripId, callback) {
+      Meteor.call('deleteTrip', tripId, callback);
+    };
+    myTripsSubscribe = function (context, $scope) {
+      var reactiveContext = $reactive(context).attach($scope);
+      var handler = reactiveContext.subscribe('my_trips', function () {
+        return [
+          reactiveContext.getReactively('date'),
+          parseInt(reactiveContext.getReactively('limit')),
+        ]
+      }, {
+        onReady: function () {
+          console.log("onReady And the Items actually Arrive");
+        },
+        onStop: function (error) {
+          if (error) {
+            console.log('An error happened - ', error);
+          } else {
+            console.log('The subscription stopped');
+          }
+        }
+      });
+      reactiveContext.helpers({
+        trips: function () {
+          return Trips.find(
+            {startAt: {$gte: reactiveContext.getReactively('date')}},
+            {limit: parseInt(reactiveContext.getReactively('limit'))}).fetch()
+        },
+        rowCount: function () {
+          return Counts.get('my_trips');
+        },
+      });
+    };
     return {
       createATrip: createATrip,
+      updateTrip: updateTrip,
+      deleteTrip: deleteTrip,
       tripSearchSubscribe: tripSearchSubscribe,
       tripDetailSubscribe: tripDetailSubscribe,
+      myTripsSubscribe: myTripsSubscribe,
     };
   }])
